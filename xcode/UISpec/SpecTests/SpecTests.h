31134dd75d205aa04767096149f4f5f40d4d5bef
Steven Solomon <solomon.steven.m@gmail.com>
2011-06-10 11:16:32 -0400

    I'm attempting to add Unit Testing to the UISpec library so I can test my futureadditions however i can't get the tests to run

---
+++ xcode/UISpec/SpecTests/SpecTests.h
@@ -0,0 +1,13 @@
+//
+//  SpecTests.h
+//  SpecTests
+//
+//  Created by Steve Solomon on 6/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SpecTests : SenTestCase
+
+@end
---
+++ xcode/UISpec/SpecTests/SpecTests.m
@@ -0,0 +1,32 @@
+//
+//  SpecTests.m
+//  SpecTests
+//
+//  Created by Steve Solomon on 6/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SpecTests.h"
+
+@implementation SpecTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SpecTests");
+}
+
+@end
